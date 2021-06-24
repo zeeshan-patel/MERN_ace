b5d641c12ece7962dc2046785d12df224fe6708a
KhushiRajpal <khushik.rajpal2015@gmail.com>
2021-06-24 19:56:36 +0530

    Database connected!!!

--- Eventia/db.js
+++ Eventia/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '#urthemusicinme123',
-    database: 'Eventia'
+    database: 'ace'
 });
 
 connection.connect((err) => {
